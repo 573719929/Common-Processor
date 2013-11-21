d95a538587b22c963f10905a720d3809afe007a7
renyulong <raynnie@qq.com>
2013-11-21 21:03:36 +0800

    a

--- CProcessor.py
+++ CProcessor.py
@@ -1,38 +1,37 @@
 # -*- coding : utf8 -*-
-
-class DataGrid:
-    def __init__(self):
-        pass
-    def __del__(self):
-        pass
-    def __iter__(self):
-        return self.next()
-    def next(self):
-        raise StopIteration
-
-class FileReader(DataGrid):
-    def __init__(self, files, fields, separator):
-        self.files, self.fields, self.separator = files, fields, separator
-    def next(self):
-        for f in self.files:
-            fi = open(f)
-            for l in fi:
-                yield dict(zip(self.fields, l.rstrip().split(self.separator)))
-            fi.close()
-        raise StopIteration
-
-
-for i in FileReader(['testdata', 'testdata1'], ['id', 'age', 'rate', 'name'], ','): print i
-class Reverse:
-    """Iterator for looping over a sequence backwards."""
-    def __init__(self, data):
-        self.data = data
-        self.index = len(data)
-    def __iter__(self):
-        return self
-    def next(self):
-        if self.index == 0:
-            raise StopIteration
-        self.index = self.index - 1
-        return self.data[self.index]
-#for i in Reverse([1,2,3]): print i
+def BOOL:
+    def __init__(self): pass
+    def __del__(self): pass
+    def __str__(self): return None
+def AND(BOOL):
+    def __init__(self, **args): self.args = args
+    def __str__(self): return '(%s)'%(' AND '.join(['(%s)'%(str(i)) for i in self.args]))
+def OR(BOOL):
+    def __init__(self, **args): self.args = args
+    def __str__(self): return '(%s)'%(' OR '.join(['(%s)'%(str(i)) for i in self.args]))
+def GT(a, b, c):
+    def __str__(self): return '(%s:%s > %s)'%(a, b, c)
+def LT(a, b, c):
+    def __str__(self): return '(%s:%s < %s)'%(a, b, c)
+def EQ(a, b, c):
+    def __str__(self): return '(%s:%s = %s)'%(a, b, c)
+def NEQ(a, b, c):
+    def __str__(self): return '(%s:%s != %s)'%(a, b, c)
+def IN(a, b, c):
+    def __str__(self): return OR(*[EQ(a, b, i) for i in c])
+def foobar(parameters):
+    ef = []
+    if 'area' in parameters:
+        ef.append(EQ('CF', 'area', parameters['area']))
+    if 'dayslot' in parameters:
+        ef.append(AND(GT('CF', 'day', parameters['dayslot'][0]), LT('CF', 'day', parameters['dayslot'][1])))
+    if 'timeslot' in parameters:
+        ef.append(OR(AND(GT('CF', 'time', parameters['timeslot'][0]), LT('CF', 'time', parameters['timeslot'][1])), AND(GT('CF', 'time', parameters['timeslot'][0]), LT('CF', 'time', parameters['timeslot'][1]))))
+    return AND(*ef)
+#
+parameters = {
+    'area' : '123',
+    'dayslot' : ['20131120', '20131130']
+    'timeslot' : ['002200', '002400']
+}
+print foobar(parameters)
